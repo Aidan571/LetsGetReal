7119519e1c8ab1da4b1fdc472bc1e280cfeb0aeb
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2020-11-22 17:15:10 -0500

    Created RealNumber class and equals method.

---
+++ RealNumber.java
@@ -0,0 +1,25 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+  public boolean equals(RealNumber other){
+    if(other.value == 0){
+      return this.value == 0;
+      }
+    else if(this.value == 0){
+      return other.value == 0;
+      }
+    else
+    return (other.value / this.value <= 1.00001 && other.value / this.value >= 0.99990) || (this.value / other.value <= 1.00001 && this.value / other.value >= 0.99990);
+  }
+}
